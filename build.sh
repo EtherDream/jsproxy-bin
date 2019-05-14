c1a8e9580c6e7922ea890f1f75c76c56a5657479
zjcqoo <zjcqoo@gmail.com>
2019-05-14 20:06:04 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-echo "compress brotli ..."
-zopfli src/brotli
-mv src/brotli.gz dist
-
 echo "compress server ..."
 brotli -f src/server.tar -o dist/server.tar.br
+
+echo "compress brotli ..."
+zopfli src/linux/brotli
+mv src/linux/brotli.gz dist/linux
