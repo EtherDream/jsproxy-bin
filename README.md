f0aa76c7e6293cc6d51a9f77ed4771f0efe7834f
zjcqoo <zjcqoo@gmail.com>
2019-05-20 16:07:25 +0800

    update

--- README.md
+++
@@ -1,7 +0,0 @@
-jsproxy 自动安装脚本
-
-# 安装
-
-```sh
-curl https://raw.githubusercontent.com/EtherDream/jsproxy/master/i.sh | sh
-```
--- build.sh
+++
@@ -1,6 +0,0 @@
-echo "compress server ..."
-brotli -f src/server.tar -o dist/server.tar.br
-
-echo "compress brotli ..."
-zopfli src/linux/brotli
-mv src/linux/brotli.gz dist/linux
